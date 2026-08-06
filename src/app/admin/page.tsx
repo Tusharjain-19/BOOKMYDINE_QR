d802591b153bd5768a791c3406d007baf64ac39f
Tushar Jain <jaint0910@gmail.com>
2026-08-06 15:38:53 +0530

    fix: removed old unused CSS imports breaking vercel build

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import "../../admin_components/index.css";
-import "../../admin_components/App.css";
 
 const AdminApp = dynamic(() => import("../../admin_components/App"), { ssr: false });
 
